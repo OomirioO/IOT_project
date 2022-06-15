4ac6e60b748721386f591e271f83a45aa008329f
miri <106790398+OomirioO@users.noreply.github.com>
2022-06-16 04:05:43 +0900

    V2
    실시간 카메라 스트리밍 웹에서 구현
    사진 캡쳐 기능 추가
    
    ※turn_off 후 다시 servo 가동 X -> turn_off 후 servo 다시 가동하도록 수정 필요

--- index.py
+++ index.py
@@ -1,64 +1,68 @@
-# 웹서버 프로그램 웹 브라우저에서 http://localhost:5000/로 접속하면 
-# index.html을 실행하고 버튼을 이용하여 서보모터, 카메라를 작동시킴
 
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, Response
 import RPi.GPIO as GPIO
 import time
 import threading
 import picamera
-from PIL import Image
+from camera import Camera
 
-servo_pin=18 #서보모터를 이용해 사료 제어
 app = Flask(__name__)
 
+servo_pin=18 #서보모터를 이용해 사료 출구 제어
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin,GPIO.OUT)
-
 servo=GPIO.PWM(servo_pin,50)
-servo.start(0)
 
 @app.route("/")
 def main():
     return render_template('index.html')
-    
 
-@app.route("/servo")                       # index.html에서 이 주소를 접속하여 해당 함수를 실행
+@app.route("/servo")         # index.html에서 이 주소를 접속하여 해당 함수를 실행
 def turn_servo():
     try:
-        servo.ChangeDutyCycle(7.5)  # 90도 서보모터 작동 -> 사료 출구 open
+        servo.start(0)
+        servo.ChangeDutyCycle(2.5)  # 0도 서보모터 작동 -> 사료 출구 open
         print("7.5")
-        time.sleep(2)         
-        servo.ChangeDutyCycle(2.5) # 0도 사료 출구 close
+        time.sleep(1)         
+        servo.ChangeDutyCycle(12.5) # 180도 사료 출구 close
         print("2.5")
-        time.sleep(2)
+        time.sleep(1)
         return "ok"                         # 함수가 'ok'문자열을 반환함
     except :
         return "fail"
 
-@app.route("/camera")
-def turn_camera():
-    try:
-        with picamera.PiCamera() as camera:
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/capture_pic')
+def capture_pic():
+    with picamera.PiCamera() as camera:
+        try:
             camera.resolution = (640, 480)
-            camera.start_preview()
-            time.sleep(1)
             camera.capture('photo.jpg')
-            camera.stop_preview()
-
-            im=Image.open('/home/pi/IOT_Python/photo.jpg') #이미지 불러오기
-            im.show()
             return "ok"
-    except :
-        return "fail"
+        except:
+            return "fail"
 
-@app.route("/stream")
-def turn_stream():
+
+@app.route("/turn_off")
+def turn_off():
     try:
+       # t1=threading.Thread(target=)
+        servo.stop()
+        #GPIO.cleanup()
         return "ok"
     except :
         return "fail"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
